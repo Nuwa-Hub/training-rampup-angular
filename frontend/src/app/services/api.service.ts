5c9dd416135e964b2b145ba953fd04a74cdef101
Nuwa-Hub <you@example.com>
2023-02-02 13:55:24 +0530

    add

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -8,40 +8,31 @@ import { PersonInterface } from "../models/person-interface";
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
-
+  private headers = new HttpHeaders().set(
+    "Content-Type",
+    "application/json; charset=utf-8"
+  );
   fetchPersondata(): Observable<PersonInterface[]> {
-    const headers = new HttpHeaders().set(
-      "Content-Type",
-      "application/json; charset=utf-8"
-    );
     return this.http
-      .get("http://localhost:5000/api/students", { headers: headers })
+      .get("http://localhost:5000/api/students", { headers: this.headers })
       .pipe(map((data: any) => data));
   }
 
   addPersondata(personData: PersonInterface): Observable<PersonInterface> {
-    const headers = new HttpHeaders().set(
-      "Content-Type",
-      "application/json; charset=utf-8"
-    );
     return this.http
       .post(
         "http://localhost:5000/api/students",
         { data: personData },
         {
-          headers: headers,
+          headers: this.headers,
         }
       )
       .pipe(map((data: any) => data));
   }
   removePersondata(id: number): Observable<PersonInterface> {
-    const headers = new HttpHeaders().set(
-      "Content-Type",
-      "application/json; charset=utf-8"
-    );
     return this.http
       .delete(`http://localhost:5000/api/students/${id}`, {
-        headers: headers,
+        headers: this.headers,
       })
       .pipe(map((data: any) => data));
   }
